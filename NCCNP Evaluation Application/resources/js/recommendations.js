4dc559fbd9e7d096ad081a8870badb39da7709b0
Audrey Bunn <37702798+acb4905@users.noreply.github.com>
2020-04-06 18:10:17 -0400

    Added 1/2 save function
    This function lets the user type their name and recommendations and adds them to the list of completed recommendations but it does not save to a text file in the saveData folder

--- NCCNP Evaluation Application/resources/js/recommendations.js
+++ NCCNP Evaluation Application/resources/js/recommendations.js
@@ -1,1 +1,31 @@
-//Page 2 javascript
+function save() {
+   var name = document.getElementById("name").value;
+
+    if (name == null) {
+        var snackbar = document.getElementById("snackbar");
+
+        // Add the "show" class to DIV
+        snackbar.className = "show";
+      
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+    }
+    else {
+        var recommendations = document.getElementsByClassName("type");
+        var responses = document.getElementsByClassName("recommendations");
+        for (var i = 0; i < recommendations.length; i++) {
+            var recommendation = recommendations[i].value;
+            var tag = document.createElement("p");
+            var text = document.createTextNode(recommendation);
+            var strong = document.createElement("strong");
+            var nameText = document.createTextNode((name + ": "));
+            strong.appendChild(nameText);
+            if (recommendation !== "") {
+                tag.appendChild(strong);
+            }
+            tag.appendChild(text);
+            responses[i].appendChild(tag);
+          }
+        // write new recommendations to the saveData file
+    }
+}
